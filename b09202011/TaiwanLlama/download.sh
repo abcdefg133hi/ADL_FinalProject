37b1531a33d2f465037c42ed0552776212d67f5e
黃偉翔 <huangweixiang@huangweixiangdeMacBook-Pro.local>
2023-12-24 11:30:01 +0800

    Finalize Model

--- b09202011/TaiwanLlama/download.sh
+++
@@ -1,3 +0,0 @@
-echo ">> Start to download the adapter model from Wei-Hsiang."
-gdown --folder https://drive.google.com/drive/folders/1Mn3u5fO4D65wqqyKXv8kl2sCC_YyaXDI
-echo ">> If successfully downloaded, the adapter will be inside the folder "TaiwanLlama"."
